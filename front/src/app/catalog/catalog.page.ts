aeb11e81741aa71b0153d417bf799cc9db83c7f0
Quillinan <milton.quillinan1798@gmail.com>
2020-02-06 23:29:27 -0300

    +Page Catalog

--- front/src/app/catalog/catalog.page.ts
+++ front/src/app/catalog/catalog.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router} from '@angular/router';
 
 @Component({
   selector: 'app-catalog',
@@ -7,7 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CatalogPage implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
+
+  favor_state:number = null;
+
+  numeros:any [] = [1,2,3,4,5]; // lista com numero de estrelas
+
+  public favor():void{ 
+    if(this.favor_state == null){
+      this.favor_state = 1;
+    }
+    else{
+      this.favor_state = null;
+    }
+  }
+
+  route_republic(){
+  	this.router.navigate(['/republic']); // Direcionamento provisório para a página republic
+  }
+
 
   ngOnInit() {
   }
